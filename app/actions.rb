543c957fbdee929b88537d64b399f0fa0a433b7c
Oliver Chen <olichen60@gmail.com>
2017-06-14 03:28:43 +0000

    added login page

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,9 @@
+helpers do
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
+end     
+
 get '/' do
     @posts = Post.order(created_at: :desc)
     erb(:index)
@@ -29,8 +35,34 @@ post '/signup' do
     
     #if user validations pass and user is saved
     if @user.save
-        "User #{username} saved!"
+        redirect to('/login')
     else
         erb(:signup)
     end
 end
+
+get '/login' do # when a GET request comes into /login
+    erb(:login) # render app/views/login.erb
+end
+
+post '/login' do
+    username = params[:username]
+    password = params[:password]
+    
+    # 1. find user by username
+    user = User.find_by(username: username)
+    
+    # 2. if that user exists
+    if user && user.password == password
+        session[:user_id] = user.id
+        redirect to('/')
+    else
+        @error_message = "Login failed."
+        erb(:login)
+    end     
+end
+
+get '/logout' do
+    session[:user_id] = nil
+    redirect to('/')
+end     
